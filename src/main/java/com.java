c21e7918729194ae7366ff56a4e1fc84ef42d583
Xieyu <790027134@qq.com>
2022-05-12 00:54:30 +0800

    first commit

---
+++ src/main/java/com.java
@@ -0,0 +1,7 @@
+/**
+ * Create by xy
+ * 2022-05-12 0:50
+ */
+
+public class com {
+}
---
+++ src/main/java/com/test.java
@@ -0,0 +1,12 @@
+/**
+ * Create by xy
+ * 2022-05-12 0:51
+ */
+
+package com;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
